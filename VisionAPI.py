8e7ba480b4a90a5595ea4ee91d4a3b0139f36147
Nozad <nozadt15@gmail.com>
2020-12-03 02:38:09 +0100

    Initial commit

---
+++ VisionAPI.py
@@ -0,0 +1,34 @@
+import io
+import os
+
+from google.cloud import vision, vision_v1
+from google.cloud.vision_v1 import types
+import pandas as pd
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'visionapiproject-297323-6be7debe0d36.json'
+client = vision.ImageAnnotatorClient()
+
+file_name = 'text.jpg'
+image_path = r'C:\Users\Nozad\PycharmProjects\VisionAPI\images\text.jpg'
+
+with io.open(image_path, 'rb') as image_file:
+    content = image_file.read()
+
+# construct an iamge instance
+image = vision_v1.types.Image(content=content)
+
+# annotate Image Response
+response = client.text_detection(image=image)  # returns TextAnnotation
+df = pd.DataFrame(columns=['locale', 'description'])
+
+texts = response.text_annotations
+for text in texts:
+    df = df.append(
+        dict(
+            locale=text.locale,
+            description=text.description
+        ),
+        ignore_index=True
+    )
+
+print(df['description'][0])
